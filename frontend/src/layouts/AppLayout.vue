6e15339615887f96c2cededdfbe7f819cc78dbc0
im-zhong <im.zhong@outlook.com>
2026-09-11 16:38:05 +0800

    fix: 平台更名为亿级科技知识图谱引擎

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -417,9 +417,9 @@ onBeforeUnmount(() => {
       />
       <aside aria-label="辅助区域 1" class="app-sidebar" :aria-hidden="isMobile && !mobileNavOpen">
         <div class="app-brand">
-          <img class="app-brand__logo" :src="logoKg" alt="知识图谱平台" />
+          <img class="app-brand__logo" :src="logoKg" alt="亿级科技知识图谱引擎" />
           <div v-if="!sidebarCollapsed" class="app-brand__name">
-            知识图谱平台
+            亿级科技知识图谱引擎
           </div>
         </div>
 
